5c4c66fa009349bbcdc76e1f9fee17f7e3381742
RohanD99 <rohan.d@technovert.com>
2023-07-12 16:27:12 +0530

    updated new files

---
+++ GettersSetters/GetterSetter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Rectangle
+{
+    private double width;    
+    public double Width
+    {
+        get { return width; }
+        set { width = value; }
+    }
+}
+
+public class Rectangles
+{
+    public double Width;
+
+    public double GetWidth()
+    {
+        return Width;
+    }
+
+    public void SetWidth(double value)
+    {
+        Width = value;
+    }
+}
+
+Rectangle rect = new Rectangle();
+rect.Width = 1;
+Console.WriteLine(rect);
+
+Rectangles rects = new Rectangles();
+rects.GetWidth;
+
+Rectangles rects2 = new Rectangles();
+rects2.SetWidth;
+
+
+
+
